f03cf2dc9ac292a2a084974642260943fb6bb911
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-06-04 19:04:18 +0200

    Renamed the main element to main-wrapper. Created App component. Added fetching of quotes from GO Quotes API, added get RandomQuote and handle click function which fetch a random quote from the loaded quote array.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quotes: [],
+      text: "",
+      author: "",
+      tag: ""
+    };
+    this.getRandomQuote = this.getRandomQuote.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  componentDidMount() {
+    // GET Quotes from Go Quotes API
+    fetch("https://goquotes-api.herokuapp.com/api/v1/all/quotes", {
+      "method": "GET",
+      "headers": {
+        "accept": "application/json"
+      },
+    })
+    .then(response => response.json())
+    .then(response => {
+      this.setState({
+        quotes: [...response.quotes]
+      });
+      this.getRandomQuote();
+    })
+    .catch(err => { console.log(err);
+    });
+  }
+
+  getRandomQuote = () => {
+    let randomQuote = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)];
+    this.setState({
+      text: randomQuote.text,
+      author: randomQuote.author,
+      tag: randomQuote.tag
+    })
+  }
+
+  handleClick = () => {
+    this.getRandomQuote();
+  }
+
+  render () {
+    return (
+
+      <div id="quote-box">
+
+          <figure>
+
+            <blockquote id="quote-text" className="no-widows">
+              {this.state.text}
+            </blockquote>
+
+            <figcaption id="author">
+              &mdash; {this.state.author}
+            </figcaption>
+
+          </figure>
+
+          <div className="buttons">
+            <div className="social">
+              <a
+                id="tweet-quote"
+                className="button"
+                href="https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text="
+              >
+                <i className="fab fa-twitter"></i>
+              </a>
+              <a
+                id="tumblr-quote"
+                className="button"
+                href="https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption="
+              >
+                <i className="fab fa-tumblr"></i>
+              </a>
+              <div id="like_button_container"></div>
+            </div>
+            <button
+              id="new-quote"
+              className="button"
+              onClick={this.handleClick}
+            >
+              New Quote
+            </button>
+          </div>
+
+        </div>
+    );
+  }
 }
 
 export default App;
