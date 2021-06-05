f4bb71101068204279845e324b06762e675c2356
5t4r8ucK <39564849+5t4r8ucK@users.noreply.github.com>
2021-06-05 15:09:51 +0200

    Removed noWidows func. Now regex applied directly to text.
    JS now adds the tag class to the text.
    
    Removed handleClick func. Can call getRandomQuote directly.
    
    Restructured state for random quotes.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import './assets/css/App.css';
 import React from 'react';
-
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       quotes: [],
-      text: "",
-      author: "",
-      tag: ""
+      randomQuote: {
+        text: "Loading quote...",
+        author: "Loading author...",
+        tag: ""
+      }
     };
     this.getRandomQuote = this.getRandomQuote.bind(this);
-    this.noWidows = this.noWidows.bind(this);
-    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -29,47 +28,36 @@ class App extends React.Component {
         quotes: [...response.quotes]
       });
       this.getRandomQuote();
-      this.noWidows();
     })
     .catch(err => { console.log(err);
     });
   }
 
   getRandomQuote = () => {
-    let randomQuote = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)];
-    this.setState({
-      text: randomQuote.text,
-      author: randomQuote.author,
-      tag: randomQuote.tag
-    })
-  }
-
-  noWidows = () => {
-    const blockquote = document.getElementById("quote-text");
-    blockquote.innerHTML = blockquote.innerHTML.replace(/\s([^\s<]{0,10})\s*$/,'&nbsp;$1');
-  }
-
-  handleClick = () => {
-    this.getRandomQuote();
+    if (this.state.quotes.length !== 0) {
+      const randomQuote = this.state.quotes[Math.floor(Math.random() * this.state.quotes.length)];
+      this.setState({
+        randomQuote: Object.assign({}, randomQuote),
+        text: randomQuote.text,
+        author: randomQuote.author,
+        tag: randomQuote.tag
+      })
+    }
   }
 
   render () {
+    const tagClass = "tag-" + this.state.randomQuote.tag;
+    const noWidowsRegex = /\s([^\s<]{0,10})\s*$/;
     return (
-
       <div id="quote-box">
-
           <figure>
-
-            <blockquote id="quote-text" className="no-widows">
-              {this.state.text}
+            <blockquote id="quote-text" className={"no-widows " + tagClass}>
+              {this.state.randomQuote.text.replace(noWidowsRegex, '\u00A0$1')}
             </blockquote>
-
-            <figcaption id="author">
-              &mdash; {this.state.author}
+            <figcaption id="author" className={tagClass}>
+              &mdash; {this.state.randomQuote.author}
             </figcaption>
-
           </figure>
-
           <div className="buttons">
             <div className="social">
               <a
@@ -91,12 +79,11 @@ class App extends React.Component {
             <button
               id="new-quote"
               className="button"
-              onClick={this.handleClick}
+              onClick={this.getRandomQuote}
             >
               New Quote
             </button>
           </div>
-
         </div>
     );
   }
